7d52d36140612e0143f3df82a1b84d31cfd598d7
alejandro pinto <apinto@buk.cl>
2026-09-15 16:37:48 -0300

    Actualiza URL publica de Apps Script

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // URL pública de la Web App de Google Apps Script.
 // No contiene contraseñas, tokens ni el ID de la planilla.
-window.RAFFLE_API_URL = 'https://script.google.com/macros/s/AKfycbwhRBa5j8HG-GzZI3SeAjV5mvu9irniuZooqEGezl5Gr2Ef8smFUf94Yl0u_uTPdodoKw/exec';
+window.RAFFLE_API_URL = 'https://script.google.com/macros/s/AKfycbw1t0xRYu6SF1mXas7xZdPFY6_6dMSKTzEVbPBltd0_thbGQ_mRBINnNjqz0JW2PbIDCA/exec';
